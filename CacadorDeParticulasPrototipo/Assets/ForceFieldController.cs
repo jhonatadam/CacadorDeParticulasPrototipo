6c3f27247a5199958dfefb18a2c1b0aabcc4d7e7
jhonatadam <jhonatamatias@hotmail.com>
2016-08-30 11:40:28 -0300

    alterado campo de força

---
+++ CacadorDeParticulasPrototipo/Assets/ForceFieldController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceFieldController : MonoBehaviour {
+
+    public GameObject fieldEffectRed;
+    public GameObject fieldEffectGreen;
+    public GameObject fieldEffectBlue;
+
+	void Update () {
+	    if (Random.value < 0.2)
+        {
+            fieldEffectRed.active = !fieldEffectRed.active;
+        }
+
+        if (Random.value < 0.2)
+        {
+            fieldEffectGreen.active = !fieldEffectGreen.active;
+        }
+
+        if (Random.value < 0.2)
+        {
+            fieldEffectBlue.active = !fieldEffectBlue.active;
+        }
+    }
+}
